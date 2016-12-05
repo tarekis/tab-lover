562e2b130db0ee402d0203189aeef65bd0d5ff09
tarekis <jackyboy@live.at>
2016-12-05 15:22:03 +0100

    Add some more spice to those lovely messages

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 window.lovelyTabMessage = (function(){
-    var hearts = ['❤','💓','💖','💗','💘','💝','💕'];
-    var heart = hearts[Math.floor(Math.random() * (hearts.length))];
-    var lang = (window && window.navigator && window.navigator.language || 'en');
-    switch(lang){
-        case 'en':
-            return 'Come back, i miss you. '+heart;
-        case 'de':
-            return 'Komm zurück, ich vermisse dich. '+heart;
-        default:
-            return 'Come back, i miss you. '+heart;
+    var stringTree = {
+        comeBackIMissYou: {
+            de: 'Komm zurück, ich vermisse dich.',
+            en: 'Come back, i miss you.'
+        }
     }
+    // Let's see what lovely options we have to build our very romantic string
+    var lovelyOptions = Object.keys(stringTree);
+    var lovelyHearts = ['❤','💓','💖','💗','💘','💝','💕'];
+    // Shuffle the dices of love and get our string base!
+    var lovelyOption = lovelyOptions[Math.floor(Math.random() * (lovelyOptions.length))];
+    var lovelyHeart = lovelyHearts[Math.floor(Math.random() * (lovelyHearts.length))];
+    // Maybe we can make our users REALLY happy by using their native language?
+    var lang = (window && window.navigator && window.navigator.userLanguage || window.navigator.language);;
+    // Now let's glue our lovely string together!
+    return ( stringTree[lovelyOption][lang] || stringTree[lovelyOption]['en'] ) + ' ' + lovelyHeart;
 })()
 window.cachedTitle = document.title;
 window.onblur = function () {
